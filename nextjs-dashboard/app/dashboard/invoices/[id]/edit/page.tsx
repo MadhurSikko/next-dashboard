2f1361ad35b459624c94c94ce011fd8a5a0e1a32
Madhur <madhur.dagar.s@gmail.com>
2024-10-16 16:18:15 +0530

    added error handling to invoice page

--- nextjs-dashboard/app/dashboard/invoices/[id]/edit/page.tsx
+++ nextjs-dashboard/app/dashboard/invoices/[id]/edit/page.tsx
@@ -1,19 +1,22 @@
-import Form from '@/app/ui/invoices/create-form';
 import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
 import { fetchInvoiceById } from '@/app/lib/data';
 import { fetchCustomers } from '@/app/lib/data';
 import EditInvoiceForm from '@/app/ui/invoices/edit-form';
+import { notFound } from 'next/navigation';
 
 export default async  function Page({params}: { params: {id: string}}) {
     // getting the id from the URL
     const id = params.id;
-    console.log(id);
     
     // fetching using promise 
     const [invoice, customers] = await Promise.all([
         fetchInvoiceById(id),
         fetchCustomers()
     ])
+
+    if (!invoice) {
+      notFound();
+    }
 
     return (
         <main>
